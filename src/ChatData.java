ba4a4c5290530ed9b9b6171aa08225a28d2600a7
IsaacX777 <153790325+IsaacX777@users.noreply.github.com>
2024-03-07 08:49:42 -0600

    files

---
+++ src/ChatData.java
@@ -0,0 +1,12 @@
+package src;
+import java.util.*;
+
+public class ChatData {
+    private ArrayList<String> users;
+    public ChatData(){
+        users = new ArrayList<>();
+    }
+    public ArrayList<String> getUsers(){
+        return users;
+    }
+}
--- src/ClientMain.java
+++ src/ClientMain.java
@@ -1,4 +1,21 @@
 package src;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class ClientMain {
+    public static void main(String[] args){
+        try{
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your name:");
+            String name
+            Socket socket = new Socket("127.0.0.1", 8001);
+            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+            CommandFromServer cfs = (CommandFromServer) is.readObject();
+            ButtonFrame frame;
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
